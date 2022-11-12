18367fd04f96a1ddc5e03445ae8e3ec39851f4e0
weihao2200669 <s.weihao@digipen.edu>
2022-11-12 13:26:38 +0800

    mainmenu
    added a mainmenu

---
+++ Cproject/Src/mainmenu.h
@@ -0,0 +1,12 @@
+/*!
+@file       mainmenu.h
+@course     csd1401 Software Engineering Project
+@section    A
+@team		RushA
+@date       31/10/2022 (last updated)
+@brief      contains declarations of Enemy functions
+*//*_________________________________________________________________________________*/
+#pragma once
+void Mainmenu_Init(void);
+void Mainmenu_Update(void);
+void Mainmenu_Exit(void);
