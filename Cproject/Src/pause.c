7af3051992ffc81668e9fa3a80f076ec62bc007b
JustinYeo-2200522 <justin.yeo@digipen.edu>
2022-11-15 00:47:02 +0800

    Removed clearbackground from pause screen (made pause screen background transparent)

--- Cproject/Src/pause.c
+++ Cproject/Src/pause.c
@@ -17,18 +17,18 @@ void pauseScreen(void)
 	red = CP_Color_Create(255, 0, 0, 255);
 	blue = CP_Color_Create(0, 0, 255, 255);
 
-	CP_Graphics_ClearBackground(CP_Color_Create(70, 70, 70, 70));
+
 	CP_Settings_Fill(CP_Color_Create(255, 0, 0, 255));
 	CP_Settings_RectMode(CP_POSITION_CENTER);
 	CP_Graphics_DrawRect((float)(CP_System_GetWindowWidth() / 2), (float)((CP_System_GetWindowHeight() / 2) - 110), 400, 90);
 	CP_Graphics_DrawRect((float)(CP_System_GetWindowWidth() / 2), (float)(CP_System_GetWindowHeight() / 2), 400, 90);
 	CP_Graphics_DrawRect((float)(CP_System_GetWindowWidth() / 2), (float)((CP_System_GetWindowHeight() / 2) + 110), 400, 90);
 	CP_Graphics_DrawRect((float)(CP_System_GetWindowWidth() / 2), (float)((CP_System_GetWindowHeight() / 2) + 220), 400, 90);
-	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
 	//	CP_Settings_TextSize(150.0f);
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
 	CP_Font_DrawText("GAME PAUSED", (float)(CP_System_GetWindowWidth() / 2), (float)(150));
 	//	CP_Settings_TextSize(150.0f);
+	CP_Settings_Fill(CP_Color_Create(0, 0, 0, 255));
 	CP_Font_DrawText("Resume", (float)(CP_System_GetWindowWidth() / 2), (float)((CP_System_GetWindowHeight() / 2) - 110));
 	CP_Font_DrawText("Retry", (float)(CP_System_GetWindowWidth() / 2), (float)(CP_System_GetWindowHeight() / 2));
 	CP_Font_DrawText("Return to Menu", (float)(CP_System_GetWindowWidth() / 2), (float)((CP_System_GetWindowHeight() / 2) + 110));
